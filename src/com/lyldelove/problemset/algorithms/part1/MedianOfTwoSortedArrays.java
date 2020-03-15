3f596c44098ccacb72da10a29efc33bdd15d8f98
lyldelove <932563352@qq.com>
2020-03-15 22:51:22 +0800

    add 4、寻找两个有序数组的中位数

---
+++ src/com/lyldelove/problemset/algorithms/part1/MedianOfTwoSortedArrays.java
@@ -0,0 +1,26 @@
+package com.lyldelove.problemset.algorithms.part1;
+
+/*
+ * 4、寻找两个有序数组的中位数
+ *
+ * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+ *
+ * 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+ *
+ * 你可以假设 nums1 和 nums2 不会同时为空。
+ */
+public class MedianOfTwoSortedArrays {
+
+    private double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // 2个数组重新排序
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2};
+        int[] nums2 = {3, 4};
+        double medianSortedArrays = new MedianOfTwoSortedArrays().findMedianSortedArrays(nums1, nums2);
+        //System.out.println(medianSortedArrays);
+        System.out.println(3/2);
+    }
+}
