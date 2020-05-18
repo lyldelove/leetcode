e2b651d907f2f082c91a4fe3c27f8229dd824a1a
lyldelove <932563352@qq.com>
2020-05-18 22:52:45 +0800

    update LC

--- src/com/lyldelove/algorithms/phase1/LC0004.java
+++ src/com/lyldelove/algorithms/phase1/LC0004.java
@@ -10,6 +10,14 @@ package com.lyldelove.algorithms.phase1;
 public class LC0004 {
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        //确保length1 <= length2
+        if(nums1.length > nums2.length) {
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+        }
+
         int length1 = nums1.length;
         int length2 = nums2.length;
         int half = (length1 + length2 + 1) / 2;
@@ -22,17 +30,46 @@ public class LC0004 {
             int i = (min + max) / 2;
             int j = half - i;
 
-            //2大
-            if(nums2[j - 1] > nums1[i]) {
+            if(i < max && nums2[j - 1] > nums1[i]) {//2大，i往右
+                min = i + 1;
+            } else if(i > min && nums1[i - 1] > nums2[j]) {//1大，i往左
+                max = i - 1;
+            } else {//满足条件
+                //取左边最大值
+                int maxLeft = 0;
+                if(i == 0) {
+                    maxLeft = nums2[j - 1];
+                } else if (j == 0) {
+                    maxLeft = nums1[i - 1];
+                } else {
+                    maxLeft = Math.max(nums1[i - 1], nums2[j - 1]);
+                }
 
+                if ( (length1 + length2) % 2 == 1 ) {
+                    return maxLeft;
+                }
+
+                //取右边最小值
+                int minRight = 0;
+                if(i == length1) {
+                    minRight = nums2[j];
+                } else if (j == length2) {
+                    minRight = nums1[i];
+                } else {
+                    minRight = Math.min(nums1[i], nums2[j]);
+                }
+
+                return (maxLeft + minRight) / 2.0;
             }
         }
         // 2个有序数组的重新排序
         // 题目要求时间复杂度为O(log(m + n))
-        return 0;
+        return 0.0;
     }
 
     public static void main(String[] args) {
-
+        int[] nums1 = {1, 3};
+        int[] nums2 = {2};
+        System.out.println(new LC0004().findMedianSortedArrays(nums1, nums2));
     }
 }
